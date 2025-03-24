ae01d926b8d5f29cc956279a2bbd46e6b46473e4
Greivin Adrián Cortés <greivincortes@gmail.com>
2025-03-24 16:11:17 -0600

    Se crea la lógica necesaria para que por medio de abstracción, el app utilice la interface. Solo faltaría la implementación del writer.

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
 
 IUserActionWriter Writer = default;
+AppLogger Logger = new AppLogger(Writer);
+Logger.WriteLog("Application started.");
