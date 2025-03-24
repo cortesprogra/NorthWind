125710f36b7e21e6e8b40e6f8da8ba3f5b65d296
Greivin Adrián Cortés <greivincortes@gmail.com>
2025-03-24 16:59:49 -0600

    Creación de un nuevo writer para escribir en consola de debug (DebugWriter).

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -3,7 +3,11 @@ using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
 
-IUserActionWriter Writer = new ConsoleWriter();
+//IUserActionWriter Writer = new ConsoleWriter();
+IUserActionWriter Writer = new DebugWriter();
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Application started.");
+
+ProductService Product = new ProductService(Writer);
+Product.Add("Demo", "Azúcar refinada.");
