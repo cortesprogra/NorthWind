98888de3275a82e2880f654e2883833390bced51
Greivin Adrián Cortés <greivincortes@gmail.com>
2025-03-24 16:38:24 -0600

    Creación de otra clase de ejemplo para evidenciar cómo se usa la interface.

---
+++ NorthWind.ConsoleApp/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.ConsoleApp.Services
+{
+    public class ProductService(IUserActionWriter Writer)
+    {
+        public void Add(string user, string productName)
+        {
+            UserAction Action = new UserAction(user, $"Created: {productName}");
+            Writer.Write( Action );
+        }
+    }
+}
