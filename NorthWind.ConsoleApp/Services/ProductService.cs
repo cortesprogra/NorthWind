15daccc8e9baf65bef1ac1403c23a09631103a8f
Greivin Adrián Cortés <greivincortes@gmail.com>
2025-03-24 16:38:59 -0600

    Creación del proyecto Writers. El "cómo lo quiero" no debe saber quien lo usa. En otras palabras el Writer no va a saber quien lo consume.

--- NorthWind.ConsoleApp/Services/ProductService.cs
+++ NorthWind.ConsoleApp/Services/ProductService.cs
@@ -12,7 +12,7 @@ namespace NorthWind.ConsoleApp.Services
     {
         public void Add(string user, string productName)
         {
-            UserAction Action = new UserAction(user, $"Created: {productName}");
+            UserAction Action = new UserAction(user, $"Created:{productName}");
             Writer.Write( Action );
         }
     }
