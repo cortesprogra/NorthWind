354e6042f0450aaab0b2dfbabc93609e3af2c11f
Greivin Adrián Cortés <greivincortes@gmail.com>
2025-03-24 17:44:00 -0600

    Se crea un HostAppllicationBuilder para indicarle cuáles servicios se requieren. Posteriormente se usa el AppHost para pedirle puntualmente una instancia de X o Y servicio. Cuando se pida la dependencia 1 devuelve esto, cuando se pida la dependencia 2 devuelve esto, etc, etc.

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,16 +1,28 @@
 ﻿
+using Microsoft.Extensions.Hosting;
 using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
+using Microsoft.Extensions.DependencyInjection;
 
-//IUserActionWriter Writer = new ConsoleWriter();
-//IUserActionWriter Writer = new DebugWriter();
-IUserActionWriter Writer = new FileWriter();
+// 1. PROCESO DE PREPARACIÓN, SE LE DICE CUÁLES SON LOS SERVICIOS QUE VAMOS A USAR
+// creame un application builder para manejar las dependencias por mi.
+HostApplicationBuilder Builder = Host.CreateApplicationBuilder();
 
-AppLogger Logger = new AppLogger(Writer);
+// Cuando te pida un Logger dame un ConsoleLogger (por ejemplo).
+Builder.Services.AddSingleton<IUserActionWriter, ConsoleWriter>();
+// Cuando te pida una instancia de AppLogger, devuelve una instancia de él mismo (no tiene abstracción).
+Builder.Services.AddSingleton<AppLogger>();
+Builder.Services.AddSingleton<ProductService>();
+
+// Ahora sí, construye el Host de la aplicación.
+using IHost AppHost = Builder.Build();
+
+// 2. CONSUMIR LOS SERVICIOS ANTES INDICADOS
+AppLogger Logger = AppHost.Services.GetRequiredService<AppLogger>();
 Logger.WriteLog("Application started.");
 
-ProductService Product = new ProductService(Writer);
+ProductService Product = AppHost.Services.GetRequiredService<ProductService>();
 Product.Add("Demo", "Azúcar refinada.");
 
 /*
