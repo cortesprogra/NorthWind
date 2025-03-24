250336f4ee698e63e6a1e6bca0e3b6d84f424c2e
Greivin Adrián Cortés <greivincortes@gmail.com>
2025-03-24 17:15:51 -0600

    Creación de un tercer writer (archivos).

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -4,10 +4,17 @@ using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
 
 //IUserActionWriter Writer = new ConsoleWriter();
-IUserActionWriter Writer = new DebugWriter();
+//IUserActionWriter Writer = new DebugWriter();
+IUserActionWriter Writer = new FileWriter();
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Application started.");
 
 ProductService Product = new ProductService(Writer);
 Product.Add("Demo", "Azúcar refinada.");
+
+/*
+ * AppLogger y los Writers: Han implementando el principio de responsabilidad única.
+ * AppLogger: Abierto pero cerrado
+ * AppLogger: Inversión de dependencias, los módulos de alto nivel son independientes de los detalles de implementación. Depende de la abstracción pero independiente de la implementación.
+ */
